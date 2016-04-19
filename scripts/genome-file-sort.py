727ed05441aefbb6d0244445c21b408ee30dc880
Brent Pedersen <bpederse@gmail.com>
2016-04-19 16:48:28 -0600

    add hg19.genome as a pilot and a script to sort genome files.

---
+++ scripts/genome-file-sort.py
@@ -0,0 +1,30 @@
+"""
+helper for getting genome files set up.
+we want lexical sort, but we want, e.g
+chr2_xxx_random to be at the bottom of the file
+here we just check for '_' and move those to the
+bottom, otherwise, we do lexical sort.
+"""
+
+import sys
+
+f = open(sys.argv[1])
+
+
+header = []
+normal = []
+weird = []
+
+for i, toks in enumerate(x.rstrip().split() for x in f if x.strip()):
+    if i == 0 and not toks[1].isdigit():
+        header.extend(toks)
+        continue
+
+    (weird if '_' in toks[0] else normal).append(tuple(toks))
+
+normal.sort()
+weird.sort()
+
+print "\t".join(header)
+print "\n".join("%s\t%s" % p for p in normal)
+print "\n".join("%s\t%s" % p for p in weird)
