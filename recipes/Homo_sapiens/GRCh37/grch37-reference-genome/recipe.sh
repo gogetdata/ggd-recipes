6d3baa4d14f44dd42704fcdee5437df763bc26be
jbelyeu <jrbelyeu@gmail.com>
2016-09-29 19:23:58 -0600

    added force option to zip in case old versions of file are laying around

--- recipes/Homo_sapiens/GRCh37/grch37-reference-genome/recipe.sh
+++ recipes/Homo_sapiens/GRCh37/grch37-reference-genome/recipe.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -eo pipefail -o nounset
-set -e
-
 # get reference file from 1000 genomes ftp site
 wget --quiet http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
 
 # unzip the reference
-bgzip -d hs37d5.fa.gz 
+bgzip -fd hs37d5.fa.gz 
 
 # index the reference
 samtools faidx hs37d5.fa
