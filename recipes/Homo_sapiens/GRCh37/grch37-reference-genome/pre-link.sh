78c840688b5873808f4c47007ab3317874f9ad37
jbelyeu <jrbelyeu@gmail.com>
2016-09-29 23:16:29 -0600

    fixed error restricting to kingspeak

--- recipes/Homo_sapiens/GRCh37/grch37-reference-genome/pre-link.sh
+++ recipes/Homo_sapiens/GRCh37/grch37-reference-genome/pre-link.sh
@@ -13,8 +13,19 @@ export RECIPE_DIR=$CONDA_ROOT/share/ggd/Homo_sapiens/GRCh37/grch37-reference-gen
 recipe_env_name="ggd_grch37-reference-genome"
 recipe_env_name="$(echo "$recipe_env_name" | sed 's/-/_/g')"
 
-activate_dir="/scratch/ucgd/lustre/u1072557/a2/etc/conda/activate.d"
-deactivate_dir="/scratch/ucgd/lustre/u1072557/a2/etc/conda/deactivate.d"
+conda_info=$(conda info --envs)
+while IFS=$'\n' read -ra conda_info; do
+    for line in "${conda_info[@]}"; do
+        if grep -q "*" <<<$line; then
+            IFS=' ' read -r -a parsed_stuff <<< "$line"
+            env_dir="${parsed_stuff[2]}"
+        fi
+    done
+done <<< "$conda_info"
+
+
+activate_dir="$env_dir/etc/conda/activate.d"
+deactivate_dir="$env_dir/etc/conda/deactivate.d"
 
 mkdir -p $activate_dir
 mkdir -p $deactivate_dir
