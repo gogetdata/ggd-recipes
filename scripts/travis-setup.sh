96bb921c7d2fcddfc47ff094ad2cf04a3d470dd4
Michael Cormier <35238534+mikecormier@users.noreply.github.com>
2018-05-09 12:59:37 -0600

    Get Conda Version

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -27,3 +27,4 @@ conda install -y conda-build anaconda-client
 pip install -U git+git://github.com/gogetdata/ggd-cli.git
 conda install -y "gsort>=0.0.2" samtools htslib zlib
 conda info -a #for debugging
+conda --version ## Get conda version 
