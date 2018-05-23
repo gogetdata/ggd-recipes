3945627b52d8ef4dceff53c16f25fafed2fac17a
Michael Cormier <35238534+mikecormier@users.noreply.github.com>
2018-05-23 14:24:44 -0600

    Use miniconda3, and install requirments.txt depen.

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -4,13 +4,13 @@ set -exo pipefail
 
 # NOTE: much of this is taken from bioconda.
 if [[ $TRAVIS_OS_NAME = "linux" ]]; then
-	curl -O https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
-	sudo bash Miniconda2-latest-Linux-x86_64.sh -b -p /anaconda/
+	curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+	sudo bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda/
 	sudo chown -R $USER /anaconda/
 	curl -Lo /anaconda/bin/check-sort-order https://github.com/gogetdata/ggd-utils/releases/download/v0.0.3/check-sort-order-linux_amd64
 else
 	curl -O https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh
-	sudo bash Miniconda2-latest-MacOSX-x86_64.sh -b -p /anaconda/
+	sudo bash Miniconda3-latest-MacOSX-x86_64.sh -b -p /anaconda/
 	sudo chown -R $USER /anaconda/
 	curl -Lo /anaconda/bin/check-sort-order https://github.com/gogetdata/ggd-utils/releases/download/v0.0.3/check-sort-order-darwin_amd64
 fi
@@ -24,8 +24,7 @@ conda config --add channels bioconda
 conda config --add channels conda-forge
 
 ## Install bioconda-utils (https://github.com/bioconda/bioconda-recipes/blob/master/.circleci/setup.sh)
-#conda install -y git pip --file https://github.com/bioconda/bioconda-utils/blob/master/bioconda_utils/bioconda_utils-requirements.txt
-while read requirement; do conda install --yes $requirement; done < git+https://github.com/bioconda/bioconda-utils/blob/master/bioconda_utils/bioconda_utils-requirements.txt
+conda install -y git pip --file https://raw.githubusercontent.com/bioconda/bioconda-utils/master/bioconda_utils/bioconda_utils-requirements.txt
 pip install git+https://github.com/bioconda/bioconda-utils.git
 
 conda install -y conda-build anaconda-client
