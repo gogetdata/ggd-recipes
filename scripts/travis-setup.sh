95ab52f0449fc5b2459d7b7c2b71b704ee5c9d9d
Michael Cormier <35238534+mikecormier@users.noreply.github.com>
2018-05-24 14:21:34 -0600

    Integrate new biconda-utils

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -24,8 +24,8 @@ conda config --add channels bioconda
 conda config --add channels conda-forge
 
 ## Install bioconda-utils (https://github.com/bioconda/bioconda-recipes/blob/master/.circleci/setup.sh)
-conda install -y git pip --file https://raw.githubusercontent.com/bioconda/bioconda-utils/master/bioconda_utils/bioconda_utils-requirements.txt
-pip install git+https://github.com/bioconda/bioconda-utils.git
+conda install -y git pip --file https://raw.githubusercontent.com/bioconda/bioconda-utils/cb3-migration/bioconda_utils/bioconda_utils-requirements.txt
+pip install git+https://github.com/bioconda/bioconda-utils/tree/cb3-migration
 
 conda install -y conda-build anaconda-client
 pip install -U git+git://github.com/gogetdata/ggd-cli.git
