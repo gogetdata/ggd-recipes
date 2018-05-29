4bbc879eca737da3e9f4483801de514e0ec80191
Michael Cormier <35238534+mikecormier@users.noreply.github.com>
2018-05-29 13:13:28 -0600

    Test bioconda-utils build --force

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -26,8 +26,7 @@ rmbuild() {
 trap rmbuild EXIT
 
 ## Test recipes using bioconda-utils (modified from https://github.com/bioconda/bioconda-recipes/blob/master/scripts/travis-run.sh)
-#bioconda-utils build recipes/ config.yaml --loglevel debug
-bioconda-utils lint recipes/ config.yaml --loglevel debug
+bioconda-utils build recipes/ config.yaml --force
 
 echo "############################################################"
 echo "############################################################"
