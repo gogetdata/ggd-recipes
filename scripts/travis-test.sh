eb0fc5d50caabfb5ffcb1bc71d5c2b6a48b42d7c
Michael Cormier <35238534+mikecormier@users.noreply.github.com>
2018-05-29 15:38:16 -0600

    Test bioconda-utils build for all current recipes
    Bioconda-utils build only supports two levels of nesting. Until this is changed, each species/build has to be tested separately.

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -26,7 +26,19 @@ rmbuild() {
 trap rmbuild EXIT
 
 ## Test recipes using bioconda-utils (modified from https://github.com/bioconda/bioconda-recipes/blob/master/scripts/travis-run.sh)
+## bioconda-utils build (biconda-utils/utils.py get_recipes only supports two levels of nesting. !!Checking if the bicondoa team can change this!!)
+##	Therefore, in order to test all recieps we have to run each species/build seperately 
+# Homo-sapien
+bioconda-utils build recipes/Homo_sapiens/GRCH37/ config.yaml --loglevel debug
 bioconda-utils build recipes/Homo_sapiens/hg19/ config.yaml --loglevel debug
+bioconda-utils build recipes/Homo_sapiens/hg38-noalt/ config.yaml --loglevel debug
+bioconda-utils /recipes/Homo_sapiens/hg38/ config.yaml --loglevel debug
+# Mus_musculus
+bioconda-utils recipes/Mus_musculus/mm10/ config.yaml --loglevel debug
+# Canis_familiaris
+bioconda-utils recipes/Canis_familiaris/canFam3/ config.yaml --loglevel debug
+
+
 
 echo "############################################################"
 echo "############################################################"
