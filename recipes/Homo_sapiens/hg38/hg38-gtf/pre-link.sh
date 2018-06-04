ba4ab3e670f7402255045814cc66cfc925c5f91a
Kittus <markasenjo@hotmail.com>
2018-06-04 14:31:53 -0400

    changed hg38-gtf directory name

--- recipes/Homo_sapiens/hg38/hg38-gtf/pre-link.sh
+++ recipes/Homo_sapiens/hg38/hg38-gtf/pre-link.sh
@@ -6,9 +6,9 @@ set -eo pipefail
 mkdir -p $PREFIX/share/ggd/Homo_sapiens/hg38/ && cd $PREFIX/share/ggd/Homo_sapiens/hg38/
 
 url=ftp://ftp.ensembl.org/pub/release-78/gtf/homo_sapiens/Homo_sapiens.GRCh38.78.gtf.gz
-mkdir -p rnaseq
+mkdir -p gtf
 remap_url=http://raw.githubusercontent.com/dpryan79/ChromosomeMappings/master/GRCh38_ensembl2UCSC.txt
 wget --quiet --no-check-certificate -qO- $remap_url | awk '{if($1!=$2) print "s/^"$1"/"$2"/g"}' > remap.sed
-wget --quiet --no-check-certificate -qO- $url | gunzip | sed -f remap.sed | grep -v "*_*_alt" > rnaseq/hg38.gtf
+wget --quiet --no-check-certificate -qO- $url | gunzip | sed -f remap.sed | grep -v "*_*_alt" > gtf/hg38.gtf
 rm remap.sed
 
