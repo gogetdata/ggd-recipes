7646df98bf73d0eb1031a6b944a97c6a4b838794
Michael Cormier <35238534+mikecormier@users.noreply.github.com>
2018-06-13 11:18:04 -0600

    Test Homo_sapiens-hg19

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -29,8 +29,8 @@ trap rmbuild EXIT
 ## bioconda-utils build (biconda-utils/utils.py get_recipes only supports two levels of nesting. !!Checking if the bicondoa team can change this!!)
 ##	Therefore, in order to test all recieps we have to run each species/build seperately 
 # Homo-sapien
-bioconda-utils build recipes/Homo_sapiens/GRCH37/ config.yaml --loglevel info
-#bioconda-utils build recipes/Homo_sapiens/hg19/ config.yaml --loglevel info
+#bioconda-utils build recipes/Homo_sapiens/GRCH37/ config.yaml --loglevel info
+bioconda-utils build recipes/Homo_sapiens/hg19/ config.yaml --loglevel info
 #bioconda-utils build recipes/Homo_sapiens/hg38-noalt/ config.yaml --loglevel info
 #bioconda-utils /recipes/Homo_sapiens/hg38/ config.yaml --loglevel info
 # Mus_musculus
