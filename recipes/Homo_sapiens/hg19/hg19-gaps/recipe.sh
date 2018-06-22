d22d8cd525a98f5f5dedcc4161b15ab4fcbd4680
Michael Cormier <35238534+mikecormier@users.noreply.github.com>
2018-06-22 15:04:53 -0600

    Update the hg19.genome location

--- recipes/Homo_sapiens/hg19/hg19-gaps/recipe.sh
+++ recipes/Homo_sapiens/hg19/hg19-gaps/recipe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eo pipefail -o nounset
-genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/hg19/hg19.genome
+genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/hg19/hg19.genome
 wget --quiet -O - http://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/gap.txt.gz \
     | gzip -dc \
     | awk -v OFS="\t" 'BEGIN {print "#chrom\tstart\tend\tsize\ttype\tstrand"} {print $2,$3,$4,$7,$8,"+"}' \
