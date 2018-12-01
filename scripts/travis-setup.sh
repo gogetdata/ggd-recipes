25d90fbe703f96209e29e86146341e92f40935c3
Michael Cormier <u1138933@kingspeak24.wasatch.peaks>
2018-11-30 18:18:50 -0700

    Configure for using Circle CI Update recipe dir

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -4,15 +4,13 @@ set -exo pipefail
 
 # NOTE: much of this is taken from bioconda.
 if [[ $TRAVIS_OS_NAME = "linux" ]]; then
-	curl -O http://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
-	sudo bash Miniconda2-latest-Linux-x86_64.sh -b -p /anaconda/
+	curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+	sudo bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda/
 	sudo chown -R $USER /anaconda/
 	curl -Lo /anaconda/bin/check-sort-order https://github.com/gogetdata/ggd-utils/releases/download/v0.0.3/check-sort-order-linux_amd64
-
-
 else
-	curl -O http://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh
-	sudo bash Miniconda2-latest-MacOSX-x86_64.sh -b -p /anaconda/
+	curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+	sudo bash Miniconda3-latest-MacOSX-x86_64.sh -b -p /anaconda/
 	sudo chown -R $USER /anaconda/
 	curl -Lo /anaconda/bin/check-sort-order https://github.com/gogetdata/ggd-utils/releases/download/v0.0.3/check-sort-order-darwin_amd64
 fi
@@ -24,7 +22,14 @@ conda install -y conda-build
 conda update -y conda
 conda config --add channels bioconda
 conda config --add channels conda-forge
-conda install -y conda-build-all --channel conda-forge
+conda install -y git pip gitpython
+
+## Install bioconda-utils (https://github.com/bioconda/bioconda-recipes/blob/master/.circleci/setup.sh)
+#conda install -y -c bioconda -c conda-forge bioconda-utils
+### Using master branch from git repo
+conda install -y --file https://raw.githubusercontent.com/bioconda/bioconda-utils/master/bioconda_utils/bioconda_utils-requirements.txt
+pip install git+https://github.com/bioconda/bioconda-utils.git
+
 conda install -y conda-build anaconda-client
 pip install -U git+git://github.com/gogetdata/ggd-cli.git
 conda install -y "gsort>=0.0.2" samtools htslib zlib
