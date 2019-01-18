b034b812fd8b3fb9ff14e6a8e39f948c98e6ff57
Michael Cormier <cormiermichaelj@gmail.com>
2019-01-17 17:03:59 -0700

    Test memmory limit

--- recipes/genomics/Homo_sapiens/GRCh37/grch37-esp-variants/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-esp-variants/recipe.sh
@@ -25,25 +25,25 @@ wget --quiet https://raw.githubusercontent.com/arq5x/gemini/master/gemini/annota
 zless ESP6500SI.all.snps_indels.vcf.gz | python sanitize-esp.py | bgzip -c > temp.gz
 tabix temp.gz
 
+# 1st clean up environment
+rm ESP6500SI-V2-SSA137.GRCh38-liftover.snps_indels.vcf.tar.gz
+rm ESP6500SI-V2-SSA137.GRCh38-liftover.chr*.snps_indels.vcf
+rm ESP6500SI.all.snps_indels.vcf.gz.tbi
+rm ESP6500SI.all.snps_indels.vcf.gz
+rm sanitize-esp.py
+
 # decompose with vt
 vt decompose -s temp.gz -o dec_temp.vcf
-echo `head dec_temp.vcf`
 echo `$reference_fasta`
 echo `head $reference_fasta`
-echo `df`
 vt normalize dec_temp.vcf -r $reference_fasta -o dec_norm_temp.vcf
 perl -pe 's/\([EA_|T|AA_]\)AC,Number=R,Type=Integer/\1AC,Number=R,Type=String/' dec_norm_temp.vcf > processed_temp.vcf
 bgzip -c processed_temp.vcf > ESP6500SI.all.snps_indels.tidy.vcf.gz
 
 tabix ESP6500SI.all.snps_indels.tidy.vcf.gz
 
-# clean up environment
-rm ESP6500SI-V2-SSA137.GRCh38-liftover.snps_indels.vcf.tar.gz
-rm ESP6500SI-V2-SSA137.GRCh38-liftover.chr*.snps_indels.vcf
 
-rm ESP6500SI.all.snps_indels.vcf.gz.tbi
-rm ESP6500SI.all.snps_indels.vcf.gz
-
+# 2nd clean up environment
 rm temp.gz
 rm temp.gz.tbi
 rm temp.vcf
@@ -51,4 +51,4 @@ rm dec_temp.vcf
 rm dec_norm_temp.vcf
 rm processed_temp.vcf
 
-rm sanitize-esp.py
+
