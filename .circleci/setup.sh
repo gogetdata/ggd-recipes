1c5f47d31f1d32d3af16fe09af67226cbcb8c73a
Michael Cormier <cormiermichaelj@gmail.com>
2019-01-31 11:42:05 -0700

    Update CI parameters

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -46,18 +46,13 @@ if [[ ! -d $WORKSPACE/anaconda ]]; then
 
 
     # step 4: install requirments from git repos
-        ## Install bioconda-utils (https://github.com/bioconda/bioconda-recipes/blob/master/.circleci/setup.sh)
+    ## Install bioconda-utils (https://github.com/bioconda/bioconda-recipes/blob/master/.circleci/setup.sh)
         # conda install -y -c bioconda -c conda-forge bioconda-utils
-    ### Using master branch from git repo
-
+        ### Using master branch from git repo
+        #conda install -y --file https://raw.githubusercontent.com/bioconda/bioconda-utils/master/bioconda_utils/bioconda_utils-requirements.txt
     conda install -y --file .circleci/requirements_bioconda_utils.txt
-    #conda install -y --file https://raw.githubusercontent.com/bioconda/bioconda-utils/master/bioconda_utils/bioconda_utils-requirements.txt
     pip install git+https://github.com/bioconda/bioconda-utils.git
     ## Install ggd-cli
-    #pip install -U git+git://github.com/gogetdata/ggd-cli.git
-    #pip install -U git+git://github.com/gogetdata/ggd-cli.git@mikec-update # Temp use mikec-update branch
-	#conda install -y --file https://raw.githubusercontent.com/mikecormier/ggd-cli/master/requirements.txt
-    #pip install -U git+git://github.com/mikecormier/ggd-cli # Temp use mikecormier ggd-cli 
     conda install -y --file https://raw.githubusercontent.com/gogetdata/ggd-cli/master/requirements.txt
     pip install -U git+git://github.com/gogetdata/ggd-cli 
 
@@ -72,12 +67,11 @@ if [[ ! -d $WORKSPACE/anaconda ]]; then
     CONDA_ROOT=$(conda info --root)
     cd $CONDA_ROOT
     curl -O https://raw.githubusercontent.com/conda-forge/conda-forge-pinning-feedstock/master/recipe/conda_build_config.yaml
-    #touch conda_build_config.yaml
     cd $cur
 
 
+    # step 7: set up local channels
     # Add local channel as highest priority
-    #conda index $WORKSPACE/anaconda/conda-bld/linux-64 $WORKSPACE/anaconda/conda-bld/osx-64 $WORKSPACE/anaconda/conda-bld/noarch
     conda index $WORKSPACE/anaconda/conda-bld/
     conda config --system --add channels file://$WORKSPACE/anaconda/conda-bld
 fi
@@ -86,6 +80,4 @@ conda config --get
 
 ls $WORKSPACE/anaconda/conda-bld
 ls $WORKSPACE/anaconda/conda-bld/noarch
-
-
 
