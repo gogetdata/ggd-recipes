bca3c922519bd03fdb9bb9d9196f7048b884ccfb
Michael Cormier <cormiermichaelj@gmail.com>
2019-03-12 16:04:09 -0600

    Add new hg38 pfam domains data recipe

---
+++ recipes/genomics/Homo_sapiens/hg38/hg38-pfam-domains-ucsc/recipe.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -eo pipefail -o nounset
+
+genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/hg38/hg38.genome
+## Get the hg38 pfam file from UCSC Genes and processes it into a bed file 
+wget --quiet -O - http://hgdownload.cse.ucsc.edu/goldenpath/hg38/database/ucscGenePfam.txt.gz \
+    | gzip -dc \
+    | cut -f 2- \
+    | awk -v OFS="\t" 'BEGIN {print "#chrom\tstart\tend\tname\tsocre\tstrand\tthickStart\tthickEnd\treserved\tblockCount\tblockSizes\tchromStarts"} {print $0}' \
+    | gsort /dev/stdin $genome \
+    | bgzip -c > pfam.bed12.bed.gz
+
+## index the bed file using tabix
+tabix pfam.bed12.bed.gz
+
