a4ac4c4a5ac50e5cea26adc46c1e7c123bedab8a
Michael Cormier <cormiermichaelj@gmail.com>
2019-03-15 18:20:30 -0600

    Update hg38 recipes with new post-link configuration

--- recipes/genomics/Homo_sapiens/hg38/hg38-cpg-islands/post-link.sh
+++ recipes/genomics/Homo_sapiens/hg38/hg38-cpg-islands/post-link.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 set -eo pipefail -o nounset
 
-export CONDA_ROOT=$(conda info --root)
+if [[ -z $(conda info --envs | grep "*" | grep -o "\/.*") ]]; then
+    export CONDA_ROOT=$(conda info --root)
+    env_dir=$CONDA_ROOT
+    export RECIPE_DIR=$CONDA_ROOT/share/ggd/Homo_sapiens/hg38/hg38-cpg-islands/1
+elif [[ $(conda info --envs | grep "*" | grep -o "\/.*") == "base" ]]; then
+    export CONDA_ROOT=$(conda info --root)
+    env_dir=$CONDA_ROOT
+    export RECIPE_DIR=$CONDA_ROOT/share/ggd/Homo_sapiens/hg38/hg38-cpg-islands/1
+else
+    env_dir=$(conda info --envs | grep "*" | grep -o "\/.*")
+    export CONDA_ROOT=$env_dir
+    export RECIPE_DIR=$env_dir/share/ggd/Homo_sapiens/hg38/hg38-cpg-islands/1
+fi
 
-PKG_DIR=`find "$CONDA_ROOT/pkgs/" -name "$PKG_NAME-$PKG_VERSION*" | grep -v ".tar.bz2" |  grep "$PKG_VERSION-.*$PKG_BUILDNUM$\|$PKG_VERSION\_.*$PKG_BUILDNUM$"`
-
-export RECIPE_DIR=$CONDA_ROOT/share/ggd/Homo_sapiens/hg38/hg38-cpg-islands/1
+PKG_DIR=`find "$CONDA_ROOT/pkgs/" -name "$PKG_NAME-$PKG_VERSION*" | grep -v ".tar.bz2" |  grep "$PKG_VERSION.*$PKG_BUILDNUM$"`
 
 if [ -d $RECIPE_DIR ]; then
     rm -r $RECIPE_DIR
@@ -16,8 +26,6 @@ mkdir -p $RECIPE_DIR
 recipe_env_name="ggd_hg38-cpg-islands"
 recipe_env_name="$(echo "$recipe_env_name" | sed 's/-/_/g')"
 
-env_dir=$(conda info --envs | grep "*" | grep -o "\/.*")
-
 activate_dir="$env_dir/etc/conda/activate.d"
 deactivate_dir="$env_dir/etc/conda/deactivate.d"
 
@@ -26,7 +34,6 @@ mkdir -p $deactivate_dir
 
 echo "export $recipe_env_name=$RECIPE_DIR" >> $activate_dir/env_vars.sh
 echo "unset $recipe_env_name">> $deactivate_dir/env_vars.sh
-ggd show-env
 
 (cd $RECIPE_DIR && bash $PKG_DIR/info/recipe/recipe.sh)
 
