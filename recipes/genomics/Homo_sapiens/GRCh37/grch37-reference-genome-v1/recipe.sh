668230ef9d7c311d84095b2fe5b939877d1d9016
Michael Cormier <cormiermichaelj@gmail.com>
2019-03-28 09:14:11 -0600

    Update grch37-reference-genome with new naming scheme

--- recipes/genomics/Homo_sapiens/GRCh37/grch37-reference-genome-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-reference-genome-v1/recipe.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -eo pipefail -o nounset
+#!/bin/sh
+set -eo pipefail -o nounset
 # get reference file from 1000 genomes ftp site
 wget --quiet http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
 
