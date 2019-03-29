fd3a4fad4203bbf0e2f8e20fa052514f3cd00fc1
Michael Cormier <cormiermichaelj@gmail.com>
2019-03-29 09:10:00 -0600

    Update UCSC hg19 reference genome fasta sequence link

--- recipes/genomics/Homo_sapiens/hg19/hg19-reference-genome-ucsc-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/hg19/hg19-reference-genome-ucsc-v1/recipe.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -eo pipefail -o nounset
 ## Get the latest hg19 reference genome for USCS. (Patch 13) 
-wget --quiet http://hgdownload.soe.ucsc.edu/goldenPath/hg19/hg19Patch13/hg19Patch13.fa.gz
+wget --quiet http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz
 
 ## Unzip file using gzip
-gzip -fd hg19Patch13.fa.gz
+gzip -fd hg19.fa.gz
 
 ## Index the fasta file using samtools 
-samtools faidx hg19Patch13.fa
+samtools faidx hg19.fa
 
 
