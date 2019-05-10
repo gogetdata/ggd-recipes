0a68e0c7c642eac10dd359bf33dbdd5bda4c2d90
Michael Cormier <cormiermichaelj@gmail.com>
2019-05-10 08:40:20 -0600

    Update recipes with new env_var rules and data naming scheme

--- recipes/genomics/Homo_sapiens/GRCh38/grch38-reference-genome-ensembl-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh38/grch38-reference-genome-ensembl-v1/recipe.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 set -eo pipefail -o nounset
-
 ## Get the GRCh38 Reference Genome release 95. This is the primary assembly file
-wget --quiet ftp://ftp.ensembl.org/pub/release-95/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+wget --quiet -O grch38-reference-genome-ensembl-v1.fa.gz ftp://ftp.ensembl.org/pub/release-95/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
 
 ## Unzip the file using GNU zip. (All files are compacted with GNU Zip for storage efficiency.) (ftp://ftp.ensembl.org/pub/release-95/fasta/homo_sapiens/dna/README)
-gzip -fd Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
-
-## Rename fasta file
-mv Homo_sapiens.GRCh38.dna.primary_assembly.fa GRCh38.primary_assembly.fa 
+gzip -fd grch38-reference-genome-ensembl-v1.fa.gz
 
 ## Index the reference genome using samtools
-samtools faidx GRCh38.primary_assembly.fa
+samtools faidx grch38-reference-genome-ensembl-v1.fa 
