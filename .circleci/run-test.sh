ad85c559589da63540e3f49e9b3a75a6826ebf1e
Michael Cormier <35238534+mikecormier@users.noreply.github.com>
2019-05-10 13:47:15 -0600

    Update run-test.sh

--- .circleci/run-test.sh
+++ .circleci/run-test.sh
@@ -82,52 +82,7 @@ for bz2 in $CHECK_DIR/*.bz2; do
 done
 
 if [[ "$cached" == true ]] ; then
-    ## Clean up build dir
-    rm $CHECK_DIR/*.bz2
-    pwd
-    bash .circleci/setup.sh
-    export PATH=/anaconda/bin:$PATH
-
-    CONDA_ROOT=$(conda info --root)
-
-    CHECK_DIR=$CONDA_ROOT/conda-bld/*
-
-    ## build the new pacakges
-    echo -e "\n-> cached dirs:\n"
-    echo "$cached_recipes_path"
-    bioconda-utils build $cached_recipes_path config.yaml
-    ## run recipe check and upload
-    for bz2 in $CHECK_DIR/*.bz2; do
-        if [[ "$(basename $bz2)" == "repodata.json.bz2" ]]; then
-            continue
-        fi
-        if [[ "$(basename $bz2)" == "*.bz2" ]]; then
-            continue
-        fi
-
-        echo "############################################################"
-        echo "-> Checking recipe" $(basename $bz2)
-        echo "############################################################"
-        ggd check-recipe $bz2
-    
-        ## Upload
-        set +o nounset
-
-        echo $CIRCLE_BRANCH
-        echo $CIRCLE_PULL_REQUEST
-
-        ## If on branch master, and there is no pull requests
-        if [[ "$CIRCLE_BRANCH" == "master" && -z "$CIRCLE_PULL_REQUEST" ]] ; then
-            if [[ "$ANACONDA_GGD_TOKEN" == "" ]]; then
-                echo -e "\n-> WARNING:"
-                echo '-> $ANACONDA_GGD_TOKEN not set'
-
-            else
-                anaconda -t $ANACONDA_GGD_TOKEN upload $bz2
-                echo -e "\n-> Successfully Uploaded\n" 
-            fi
-        fi
-    done
+    bash .circleci/check_cached_recipes.sh
 fi
 
 if [[ "$recipe_uploaded" == true ]] ; then
