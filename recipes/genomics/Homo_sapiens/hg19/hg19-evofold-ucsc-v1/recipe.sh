e1ed222ac95133e759c64fcac7b9ee47ed186217
Michael Cormier <cormiermichaelj@gmail.com>
2019-05-13 11:00:48 -0600

    New hg19 evofold data recipe

---
+++ recipes/genomics/Homo_sapiens/hg19/hg19-evofold-ucsc-v1/recipe.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eo pipefail -o nounset
+
+genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/hg19/hg19.genome
+
+wget --quiet -O - http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/evofold.txt.gz \
+    | gzip -dc \
+    | awk -v OFS="\t" 'BEGIN {print "#chrom\tstart\tend\tsize\tscore\tstrand\tmRNAsecstruct\tsecstrcut_conf"} {print $2,$3,$4,$8,$6,$7,$9,$10}' \
+    | gsort /dev/stdin $genome \
+    | bgzip -c > hg19-evofold-ucsc-v1.bed.gz
+
+tabix hg19-evofold-ucsc-v1.bed.gz
