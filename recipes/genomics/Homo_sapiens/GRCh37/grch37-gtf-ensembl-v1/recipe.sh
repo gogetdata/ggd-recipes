4f67c34183c4ee3a13a86b0e091b0e8bc24b43db
Michael Cormier <cormiermichaelj@gmail.com>
2019-05-17 13:51:04 -0600

    New grch37 recipe. (The gtf file from ensembl)

---
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-gtf-ensembl-v1/recipe.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eo pipefail -o nounset
+
+genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/GRCh37/GRCh37.genome
+
+wget -quiet -O - ftp://ftp.ensembl.org/pub/release-75/gtf/homo_sapiens/Homo_sapiens.GRCh37.75.gtf.gz \
+    | gzip -dc \
+    | gsort /dev/stdin/ $genome \
+    | bgzip -c > grch37-gtf-ensembl-v1.vcf.gz
+
+tabix grch37-gtf-ensembl-v1.vcf.gz
