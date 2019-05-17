a8832cede7a7ff7483b1643db9ccd8b15aeb9d47
Michael Cormier <cormiermichaelj@gmail.com>
2019-05-17 13:50:28 -0600

    New grch37 recipe. (The dbsnp vcf file version 138 hosted on the broad's ftp site)

--- recipes/genomics/Homo_sapiens/GRCh37/grch37-dbsnp-138-broad-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-dbsnp-138-broad-v1/recipe.sh
@@ -6,6 +6,6 @@ genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Ho
 wget --quiet -O - ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/dbsnp_138.b37.vcf.gz \
     | gzip -dc \
     | gsort /dev/stdin $genome \
-    | bgzip -c > grch37-dpsnp-138-braod-v1.vcf.gz \
+    | bgzip -c > grch37-dpsnp-138-braod-v1.vcf.gz 
 
 tabix grch37-dpsnp-138-braod-v1.vcf.gz 
