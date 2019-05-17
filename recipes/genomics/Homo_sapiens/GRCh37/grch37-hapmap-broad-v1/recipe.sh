ee25a79cdc255a9e31a0d8b39046237b1bd410b0
Michael Cormier <cormiermichaelj@gmail.com>
2019-05-17 12:38:00 -0600

    New grch37 recipe. (The hapmap vcf hosted on the broad's ftp site)

--- recipes/genomics/Homo_sapiens/GRCh37/grch37-hapmap-broad-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-hapmap-broad-v1/recipe.sh
@@ -5,7 +5,7 @@ set -eo pipefail -o nounset
 
 genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/GRCh37/GRCh37.genome
 
-wget --queit -O - ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/hapmap_3.3.b37.vcf.gz \
+wget --quiet -O - ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/hapmap_3.3.b37.vcf.gz \
     | gzip -dc \
     | gsort /dev/stdin $genome \
     | bgzip -c > grch37-hapmap-broad-v1.vcf.gz \
