c712c17283b22e0624f5829320e54988e00a5456
Michael Cormier <cormiermichaelj@gmail.com>
2019-05-17 12:37:12 -0600

    New grch37 recipe. (The 1000g phase1 high-confidnece snps hosted on the broad's ftp site)

--- recipes/genomics/Homo_sapiens/GRCh37/grch37-1000g-snps-phase1-broad-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-1000g-snps-phase1-broad-v1/recipe.sh
@@ -3,7 +3,7 @@ set -eo pipefail -o nounset
 
 genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/GRCh37/GRCh37.genome
 
-wget --queit -O - ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/1000G_phase1.snps.high_confidence.b37.vcf.gz 
+wget --quiet -O - ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/b37/1000G_phase1.snps.high_confidence.b37.vcf.gz 
     | gzip -dc \
     | gsort /dev/stdin $genome \
     | bgzip -c > grch37-1000g-snps-phase1-broad-v1.vcf.gz \
