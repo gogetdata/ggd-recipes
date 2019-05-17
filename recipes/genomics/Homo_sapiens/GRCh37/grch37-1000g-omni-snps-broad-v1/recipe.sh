d5ae9d14f353a92625e74b7d9ec6ed0e8ece8970
Michael Cormier <cormiermichaelj@gmail.com>
2019-05-17 13:49:45 -0600

    New grch37 recipe. (The 1000g omni snps hosted on the broad's ftp site)

--- recipes/genomics/Homo_sapiens/GRCh37/grch37-1000g-omni-snps-broad-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-1000g-omni-snps-broad-v1/recipe.sh
@@ -5,6 +5,6 @@ genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Ho
 wget --quiet -O - ftp://gsapubftp-anonymous:none@ftp.broadinstitute.org/bundle/b37/1000G_omni2.5.b37.vcf.gz \
     | gzip -dc \
     | gsort /dev/stdin $genome \
-    | bgzip -c > grch37-1000g-omni-snps-braod-v1.vcf.gz \
+    | bgzip -c > grch37-1000g-omni-snps-braod-v1.vcf.gz 
 
 tabix grch37-1000g-omni-snps-braod-v1.vcf.gz 
