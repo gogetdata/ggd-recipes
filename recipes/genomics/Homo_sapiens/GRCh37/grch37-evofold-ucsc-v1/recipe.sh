50f7d058990206831aac50838b3f0647764ab78d
Michael Cormier <cormiermichaelj@gmail.com>
2019-06-18 11:16:50 -0600

    Update evofold bed file to include UCSC event name

--- recipes/genomics/Homo_sapiens/GRCh37/grch37-evofold-ucsc-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-evofold-ucsc-v1/recipe.sh
@@ -13,7 +13,7 @@ wget --quiet -O - https://raw.githubusercontent.com/dpryan79/ChromosomeMappings/
 ## Get hg19 evofold file
 wget --quiet -O - http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/evofold.txt.gz \
     | gzip -dc \
-    | awk -v OFS="\t" 'BEGIN {print "#chrom\tstart\tend\tsize\tscore\tstrand\tmRNAsecstruct\tsecondary_struct_conf"} {print $2,$3,$4,$8,$6,$7,$9,$10}' \
+    | awk -v OFS="\t" 'BEGIN {print "#chrom\tstart\tend\tUCSC_event_name\tsize\tscore\tstrand\tmRNAsecstruct\tsecondary_struct_conf"} {print $2,$3,$4,$5,$8,$6,$7,$9,$10}' \
     | sed -f remap.sed \
     | gsort /dev/stdin $genome \
     | bgzip -c > grch37-evofold-ucsc-v1.bed.gz
