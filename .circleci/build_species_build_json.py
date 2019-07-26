67cf5f9b9611f3c0d891ffb1b26b64291f909489
Michael Cormier <cormiermichaelj@gmail.com>
2019-07-26 12:32:44 -0600

    New approach to update species and genome build metadata files during nightly build

---
+++ .circleci/build_species_build_json.py
@@ -0,0 +1,118 @@
+from __future__ import print_function
+import os
+import argparse
+from git import Repo
+import shutil
+import json
+
+
+#------------------------------------------------------------------------------------------------
+# Argument Parser
+#------------------------------------------------------------------------------------------------
+parser = argparse.ArgumentParser(description="Get the species and associated genome builds available in ggd")
+parser.add_argument("-rp", "--repo_path", help="The file path to the ggd-recipe repo")
+parser.add_argument("-t", "--tmp_dir", required=True, help="The temporary directory to store the ggd metadata repo in")
+
+args = parser.parse_args()
+
+
+#------------------------------------------------------------------------------------------------
+# File paths
+#------------------------------------------------------------------------------------------------
+GENOME_DIR = os.path.join(args.repo_path,"genomes")
+METADATA_GITHUB_URL = "git@github.com:gogetdata/ggd-metadata.git"
+
+
+#------------------------------------------------------------------------------------------------
+# Methods/Functions
+#------------------------------------------------------------------------------------------------
+
+def get_builds(species):
+    """Get the genome_builds for a specific species
+
+    get_builds
+    ==========
+    This method is used to get the genome builds associated with a specific species available in the ggd-recipes repo. 
+    
+    Parameters:
+    -----------
+    1) species: The species in which to get the available genome builds for
+
+    Returns:
+    1) A list of available genome builds for the specified species
+    """
+
+    ## Get the species directory 
+    species_dir = os.path.join(GENOME_DIR,species)
+
+    ## Make sure the directory exists
+    assert os.path.isdir(species_dir)
+
+    ## Get all available genome builds for that species
+    return(os.listdir(species_dir))
+
+
+#------------------------------------------------------------------------------------------------
+# Main
+#------------------------------------------------------------------------------------------------
+
+## Get unique species set
+species_set = set(os.listdir(GENOME_DIR))
+
+## Get genome builds for each species
+species_dict = dict() ## Dictionary with key = species, value = list of genome builds
+build_dict = dict() ## Dictionary with key = genome_build, value = species
+for s in species_set:
+    genome_builds = get_builds(s)
+    species_dict[s] = genome_builds
+    for b in genome_builds:
+        build_dict[b] = s
+
+## Write json output
+tmp_dir = os.path.join(os.getcwd(),"species_and_genome_build/")
+print("\n-> Making a tmp directory to store species and genome build json files: %s" %tmp_dir)
+if not os.path.exists(tmp_dir):
+    os.makedirs(tmp_dir)
+
+print("\n\t-> Writing species_to_build.json")
+species_file_path = os.path.join(tmp_dir,"species_to_build.json")  
+with open(species_file_path, "w") as sf:
+    json.dump(species_dict,sf)
+
+print("\n\t-> Writing build_to_species.json")
+build_file_path = os.path.join(tmp_dir,"build_to_species.json") 
+with open(build_file_path, "w") as gf:
+    json.dump(build_dict,gf)
+
+## Get current working dir
+cwd = os.getcwd()
+
+## Make metadata_repo_dir
+metadata_repo_dir = os.path.join(args.tmp_dir,"ggd-metadata")
+if not os.path.isidr(metadata_repo_dir):
+    os.makedirs(metadata_repo_dir)
+
+Repo.clone_from(METADATA_GITHUB_URL,metadata_repo_dir)
+os.chdir(metadata_repo_dir)
+Repo(tmp_repo_dir).remotes.origin.pull()
+repo = Repo(metadata_repo_dir)
+dest = os.path.join(metadata_repo_dir,"species_and_build")
+shutil.copy(species_file_path, dest)
+shuilt.copy(build_file_path, dest)
+
+## Add, commit, and push to ggd-metadata repo
+sp.check_call(["git", "config", "user.email", "CIRCLECI@circleci.com"])
+sp.check_call(["git", "config", "user.name", "CIRCLECI"])
+repo.git.add("species_and_build/species_to_build.json")
+repo.git.add("species_and_build/build_to_species.json")
+from datetime import datetime
+date_time = datetime.today().isoformat()
+repo.git.commit("-m", "Update species_to_build.json and build_to_species.json files. ({date})".format(date=date_time))
+repo.git.push()
+
+print("\n-> Successfuly pushed new species and build json files to ggd-metadata repo")
+
+## Retrun to cwd
+os.chdir(cwd)
+
+
