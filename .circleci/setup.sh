69928edacb9c1f5078537827719e40bad87b83b6
Michael Cormier <cormiermichaelj@gmail.com>
2019-10-11 07:01:31 -0600

    Temporarily use the cli_update branch of ggd-cli

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -51,7 +51,10 @@ if [[ ! -d $WORKSPACE/anaconda ]]; then
     conda install -y --file https://raw.githubusercontent.com/bioconda/bioconda-utils/$BIOCONDA_UTILS_TAG/bioconda_utils/bioconda_utils-requirements.txt
     pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG
     ## Install ggd-cli
-    pip install -U git+git://github.com/gogetdata/ggd-cli 
+    #pip install -U git+git://github.com/gogetdata/ggd-cli 
+
+    ## Temporarily use the cli_update 
+    pip install -U git+git://github.com/gogetdata/ggd-cli@cli_update 
 
 
     # step 5: cleanup
