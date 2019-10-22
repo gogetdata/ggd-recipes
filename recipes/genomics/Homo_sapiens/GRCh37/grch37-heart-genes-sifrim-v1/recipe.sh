be5bf75138abfd295e8f6eb51e5b35959a7011dd
Michael Cormier <cormiermichaelj@gmail.com>
2019-10-22 06:51:21 -0600

    Update heart gene recipe

--- recipes/genomics/Homo_sapiens/GRCh37/grch37-heart-genes-sifrim-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-heart-genes-sifrim-v1/recipe.sh
@@ -33,36 +33,91 @@ python pyscript.py ng.3627-S13.xlsx
 
 grch37_gtf="$(ggd get-files grch37-gtf-ensembl-v1 -s 'Homo_sapiens' -g 'GRCh37' -p 'grch37-gtf-ensembl-v1.gtf.gz')"
 #only grabs protein coding sequence, CDS or stop_codon (separate from CDS for some reason) | then turns 1-based GFF format into 0-based half-open, BED format | cuts out necessary columns, sorts by gene
-zgrep 'protein_coding\tstop_codon\|protein_coding\tCDS' $grch37_gtf \
-    | awk '{$4=$4-1; print $0}' OFS='\t' \
+zless $grch37_gtf \
+    | awk 'BEGIN { OFS="\t"} {if ( $3 == "CDS" || $3 == "stop_codon" ) print $0}' \
+    | awk 'BEGIN { OFS="\t"} {$4=$4-1; print $0}' \
     | cut -f 1,4,5,12,16 \
     | sed 's/\"//g' \
-    | sed 's/;//g' > Homo_sapiens37.bed
+    | sed 's/;//g' \
+    | sed 's/ /\t/g' > coding_gene_file.bed
     
-# matches gene names to bed coordinates
-awk 'FNR==NR{genes[$1]; next} {for (gene in genes) if (gene == $5) print $0, genes[gene]}' \
-        FS='\t' \
-        OFS='\t' grch37-heart-genes-sifrim-v1.tsv Homo_sapiens37.bed \
-    | gsort /dev/stdin $genome_loc \
-    | cut -f -5 > unflattened_grch37-heart-genes-sifrim-v1.bed
+
+#######################################################################################################
+cat << EOF > parse_gene.py
+
+"""
+Get a list of genome coordinates for a list of ad genes
+"""
+import sys 
+
+tsv_file = sys.argv[1] ## A single column tsv file 
+coding_genes_file = sys.argv[2] ## A bed file with 5 columns: 1: chrom, 2: start, 3: end, 4: transcript, 5: gene
+outfile = sys.argv[3] ## File to write to
+
+## Get a dict with keys = genes, values = empty []
+with open(tsv_file, "r") as ad: 
+    gene_dict = {x.strip():[] for x in ad} 
+
+## Add entries from coding gene file to dict based on gene
+with open(coding_genes_file) as cg: 
+    for line in cg: 
+        line_list = line.strip().split("\t")
+        gene = line_list[4] 
+        if gene in gene_dict:
+            gene_dict[gene].append(line)
+
+## Write dict out
+with open(outfile, "w") as o:
+    for gene, coor in gene_dict.items():
+        o.write("".join(coor))
+
+EOF
+python parse_gene.py grch37-heart-genes-sifrim-v1.tsv coding_gene_file.bed unflattened_grch37-heart-genes-sifrim-v1.bed 
+
+#######################################################################################################
+
+cat << EOF > sort_columns.py
+"""
+sort the transcript id column
+sort and get a unique list of the gene column
+""" 
+import sys
+for line in sys.stdin.readlines():
+    line_list = line.strip().split("\t")
+    ## Sort column 4
+    line_list[3] = ",".join(sorted(line_list[3].strip().split(",")))
+    ## Sort column 5 and get a uniqe list
+    line_list[4] = ",".join(sorted(list(set(line_list[4].strip().split(",")))))
+
+    ## Print to stdout
+    print("\t".join(line_list))
+
+EOF
+
+#######################################################################################################
 
 # creates flattened representation of protein-coding exome covering AD genes
-bedtools merge -c 4,5 -o collapse -i unflattened_grch37-heart-genes-sifrim-v1.bed \
-    | awk -v OFS="\t" 'BEGIN {print "#chrom\tstart\tend\tgene-id\tgene-name"} {print $0}' \
-    | gsort /dev/stdin $genome_loc \
+gsort unflattened_grch37-heart-genes-sifrim-v1.bed $genome \
+    | bedtools merge -c 4,5 -o collapse -i - \
+    | awk -v OFS="\t" 'BEGIN {print "#chrom\tstart\tend\ttranscript_ids\tgene_name"} {print $0}' \
+    | python sort_columns.py \
+    | gsort /dev/stdin $genome \
     | bgzip -c > grch37-heart-genes-sifrim-v1.bed.gz
 tabix grch37-heart-genes-sifrim-v1.bed.gz
 
 # bedtools complement so we can use the EXCLUDE option
 bedtools complement -i grch37-heart-genes-sifrim-v1.bed.gz -g $genome_loc \
+    | gsort /dev/stdin $genome \
+    | sed "1d" \
     | awk -v OFS="\t" 'BEGIN {print "#chrom\tstart\tend"} {print $0}' \
-    | gsort /dev/stdin $genome_loc \
     | bgzip -c > grch37-heart-genes-sifrim-v1.complement.bed.gz
 tabix -f grch37-heart-genes-sifrim-v1.complement.bed.gz
 
-rm GRCh37.genome
-rm Homo_sapiens37.bed
 rm grch37-heart-genes-sifrim-v1.tsv
 rm unflattened_grch37-heart-genes-sifrim-v1.bed
 rm ng.3627-S13.xlsx
 rm pyscript.py
+rm $genome_loc
+rm parse_gene.py
+rm sort_columns.py
+rm coding_gene_file.bed
