48d915af964656845c6fafde1c7ee2101c0b0fc6
Jonathan Belyeu <jrbelyeu@gmail.com>
2019-11-09 17:38:54 -0700

    added recipes for chr22 pacbio data from giab

---
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-hg004-pb-chr22-giab-v1/recipe.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eo pipefail -o nounset
+set -eo
+#download HG002 data, convert to fastq, align with minimap, call SVs
+wget -q ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/AshkenazimTrio/HG004_NA24143_mother/PacBio_MtSinai_NIST/CSHL_bwamem_bam_GRCh37/BWA-MEM_Chr22_HG004_merged_11_12.sort.bam
+bedtools bamtofastq -i BWA-MEM_Chr22_HG004_merged_11_12.sort.bam -fq chr22_pb_HG004_mom.fastq
+rm BWA-MEM_Chr22_HG004_merged_11_12.sort.bam
+bgzip chr22_pb_HG004_mom.fastq
