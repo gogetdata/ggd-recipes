a1d7489a2ef15d10852c1ba00de6c0c6ed924e1d
jbelyeu <jrbelyeu@gmail.com>
2019-11-12 11:30:39 -0500

    fixed indexing

--- recipes/genomics/Homo_sapiens/GRCh37/grch37-hg002-pb-chr22-giab-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-hg002-pb-chr22-giab-v1/recipe.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eo pipefail -o nounset
-set -eo
-#download HG002 data, convert to fastq, align with minimap, call SVs
+#download HG002 data, convert to fastq
 wget -q ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/AshkenazimTrio/HG002_NA24385_son/PacBio_MtSinai_NIST/CSHL_bwamem_bam_GRCh37/BWA-MEM_Chr22_HG002_merged_11_12.sort.bam
-bedtools bamtofastq -i BWA-MEM_Chr22_HG002_merged_11_12.sort.bam -fq chr22_pb_HG002_kid.fastq
+samtools fastq BWA-MEM_Chr22_HG002_merged_11_12.sort.bam > chr22_pb_HG002_kid.fastq
 rm BWA-MEM_Chr22_HG002_merged_11_12.sort.bam
 bgzip chr22_pb_HG002_kid.fastq
+samtools faidx chr22_pb_HG002_kid.fastq.gz
--- recipes/genomics/Homo_sapiens/GRCh37/grch37-hg003-pb-chr22-giab-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-hg003-pb-chr22-giab-v1/recipe.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eo pipefail -o nounset
-set -eo
-#download HG003 data, convert to fastq, align with minimap, call SVs
+#download HG003 data, convert to fastq
 wget -q ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/AshkenazimTrio/HG003_NA24149_father/PacBio_MtSinai_NIST/CSHL_bwamem_bam_GRCh37/BWA-MEM_Chr22_HG003_merged_11_12.sort.bam
-bedtools bamtofastq -i BWA-MEM_Chr22_HG003_merged_11_12.sort.bam -fq chr22_pb_HG003_dad.fastq
+samtools fastq BWA-MEM_Chr22_HG003_merged_11_12.sort.bam > chr22_pb_HG003_dad.fastq
 rm BWA-MEM_Chr22_HG003_merged_11_12.sort.bam
 bgzip chr22_pb_HG003_dad.fastq
+samtools faidx chr22_pb_HG003_dad.fastq.gz
