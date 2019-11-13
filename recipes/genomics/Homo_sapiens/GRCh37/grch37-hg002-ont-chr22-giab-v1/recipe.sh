df42d20f3c1340f3918dde7657a59328f5fcf5a8
Michael Cormier <35238534+mikecormier@users.noreply.github.com>
2019-11-13 14:38:38 -0500

    Update recipe.sh
    Change batch size (Default == 500m)

--- recipes/genomics/Homo_sapiens/GRCh37/grch37-hg002-ont-chr22-giab-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-hg002-ont-chr22-giab-v1/recipe.sh
@@ -3,7 +3,7 @@ set -eo pipefail -o nounset
 reference=$(ggd get-files grch37-reference-genome-ensembl-v1 -s 'Homo_sapiens' -g "GRCh37" -p "*.fa")
 
 wget -q ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/AshkenazimTrio/HG002_NA24385_son/CORNELL_Oxford_Nanopore/giab.hg002.2D.fastq
-minimap2 -t 1 -ax map-ont $reference giab.hg002.2D.fastq \
+minimap2 -k 100m -t 1 -ax map-ont $reference giab.hg002.2D.fastq \
     | samtools view -hSb /dev/stdin \
     | samtools sort /dev/stdin \
     > giab.hg002.ont.bam
