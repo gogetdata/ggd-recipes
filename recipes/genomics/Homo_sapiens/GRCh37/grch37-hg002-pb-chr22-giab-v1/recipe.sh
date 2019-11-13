89dff5048cfeeed952baead121f8aec0e5f4872c
Jon Belyeu <jrbelyeu@gmail.com>
2019-11-13 10:46:12 -0500

    added recipes for chr22 pacbio data from giab (#86)
    * added recipes for chr22 pacbio data from giab
    
    * fixed indexing

---
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-hg002-pb-chr22-giab-v1/recipe.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eo pipefail -o nounset
+#download HG002 data, convert to fastq
+wget -q ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/AshkenazimTrio/HG002_NA24385_son/PacBio_MtSinai_NIST/CSHL_bwamem_bam_GRCh37/BWA-MEM_Chr22_HG002_merged_11_12.sort.bam
+samtools fastq BWA-MEM_Chr22_HG002_merged_11_12.sort.bam > chr22_pb_HG002_kid.fastq
+rm BWA-MEM_Chr22_HG002_merged_11_12.sort.bam
+bgzip chr22_pb_HG002_kid.fastq
+samtools faidx chr22_pb_HG002_kid.fastq.gz
