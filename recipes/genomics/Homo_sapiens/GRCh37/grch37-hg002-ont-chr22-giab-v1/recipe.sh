f36c9b2a8c07b2d5d3c6321ad945a1aaeac0e9d8
jbelyeu <jrbelyeu@gmail.com>
2019-11-13 13:55:30 -0500

    switch to grch37 for consistency

--- recipes/genomics/Homo_sapiens/GRCh37/grch37-hg002-ont-chr22-giab-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-hg002-ont-chr22-giab-v1/recipe.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 set -eo pipefail -o nounset
-reference=$(ggd get-files hg19-reference-genome-ucsc-v1 -s 'Homo_sapiens' -g 'hg19' -p '*.fa')
+reference=$(ggd get-files grch37-reference-genome-ensembl-v1 -s 'Homo_sapiens' -g "GRCh37" -p "*.fa")
 
 wget -q ftp://ftp-trace.ncbi.nih.gov/giab/ftp/data/AshkenazimTrio/HG002_NA24385_son/CORNELL_Oxford_Nanopore/giab.hg002.2D.fastq
 minimap2 -t 1 -ax map-ont $reference giab.hg002.2D.fastq \
     | samtools view -hSb /dev/stdin \
     | samtools sort /dev/stdin \
     > giab.hg002.ont.bam
-rm giab.hg002.2D.fastq
 
 samtools index giab.hg002.ont.bam
-samtools view -hb giab.hg002.ont.bam "chr22" > chr22_hg002_ont.bam
+samtools view -hb giab.hg002.ont.bam "22" > chr22_hg002_ont.bam
+samtools index chr22_hg002_ont.bam
+
+#clean up
+rm giab.hg002.2D.fastq
 rm giab.hg002.ont.bam
 rm giab.hg002.ont.bam.bai
-samtools index chr22_hg002_ont.bam
