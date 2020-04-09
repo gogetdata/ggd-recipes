2362658288d73ea830ea89af01113fde11620fcd
Michael Cormier <cormiermichaelj@gmail.com>
2020-04-09 13:02:19 -0600

    Assembly Gap Recipes - updated hg19-gaps-ucsc-v1 - update hg38-gaps-ucsc-v1 - new grch37-gaps-ucsc-v1 (Remmapped from hg19) - new grch38-gaps-ucsc-v1 (Remapped from hg38)

--- recipes/genomics/Homo_sapiens/hg38/hg38-gaps-ucsc-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/hg38/hg38-gaps-ucsc-v1/recipe.sh
@@ -4,7 +4,7 @@ set -eo pipefail -o nounset
 genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/hg38/hg38.genome
 wget --quiet -O - http://hgdownload.cse.ucsc.edu/goldenpath/hg38/database/gap.txt.gz \
     | gzip -dc \
-    | awk -v OFS="\t" 'BEGIN {print "#chrom\tstart\tend\tsize\ttype\tstrand"} {print $2,$3,$4,$7,$8,"+"}' \
+    | awk -v OFS="\t" 'BEGIN {print "#Table Info: https://genome.ucsc.edu/cgi-bin/hgTables?db=hg38&hgta_group=map&hgta_track=gap&hgta_table=gap&hgta_doSchema=describe+table+schema\n#chrom\tstart\tend\tsize\ttype\tstrand"} {print $2,$3,$4,$7,$8,"+"}' \
     | gsort /dev/stdin $genome \
     | bgzip -c > hg38-gaps-ucsc-v1.bed.gz
 
