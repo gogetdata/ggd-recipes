d021330bfe2cc9a4d9706dce7f6cd7cdac5adb37
Michael Cormier <cormiermichaelj@gmail.com>
2020-04-15 17:07:25 -0600

    Update Pfam Domain Recipes

--- recipes/genomics/Homo_sapiens/hg38/hg38-pfam-domains-ucsc-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/hg38/hg38-pfam-domains-ucsc-v1/recipe.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -eo pipefail -o nounset
+
 genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/hg38/hg38.genome
 ## Get the hg38 pfam file from UCSC Genes and processes it into a bed file 
 wget --quiet -O - http://hgdownload.cse.ucsc.edu/goldenpath/hg38/database/ucscGenePfam.txt.gz \
     | gzip -dc \
     | cut -f 2- \
-    | awk -v OFS="\t" 'BEGIN {print "#chrom\tstart\tend\tname\tsocre\tstrand\tthickStart\tthickEnd\treserved\tblockCount\tblockSizes\tchromStarts"} {print $0}' \
+    | awk -v OFS="\t" 'BEGIN {print "#Table Info: https://genome.ucsc.edu/cgi-bin/hgTables?db=hg38&hgta_group=genes&hgta_track=ucscGenePfam&hgta_table=ucscGenePfam&hgta_doSchema=describe+table+schema\n#chrom\tstart\tend\tname\tsocre\tstrand\tthickStart\tthickEnd\treserved\tblockCount\tblockSizes\tchromStarts"} {print $0}' \
     | gsort /dev/stdin $genome \
     | bgzip -c > hg38-pfam-ucsc-v1.bed12.bed.gz
 
