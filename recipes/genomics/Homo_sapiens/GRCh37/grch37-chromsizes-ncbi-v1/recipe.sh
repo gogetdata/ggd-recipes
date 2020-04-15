7cac76df1d9438244d189e58550ec159f35f5e3f
Michael Cormier <cormiermichaelj@gmail.com>
2020-04-15 09:44:23 -0600

    Update Homo_sapiens genome files New chromsize files to create genome files from NCBI file

---
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-chromsizes-ncbi-v1/recipe.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+set -eo pipefail -o nounset
+#!/bin/sh
+set -eo pipefail -o nounset
+
+# -------------------------------------------------------------------------------------------------------
+# header fields: 
+# Column 1: $_Sequence_Name
+# Column 2: $Sequence_Role 
+# Column 3: $Assigned_Molecule 
+# Column 4: $Assigned_Molecule_Location_Type 
+# Column 5: $GenBank_Accn 
+# Column 6: $Relationship 
+# Column 7: $RefSeq_Accn 
+# Column 8: $Assembly_Unit 
+# Column 9: $Sequence_Length 
+# Column 10: $UCSC_style_name  
+# -------------------------------------------------------------------------------------------------------
+
+## Get the report file from NCBI
+wget --quiet ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.14_GRCh37.p13/GCA_000001405.14_GRCh37.p13_assembly_report.txt 
+
+## Process the report  file
+cat <(grep "^#" GCA_000001405.14_GRCh37.p13_assembly_report.txt | tail -n 1) <(grep -v "^#" GCA_000001405.14_GRCh37.p13_assembly_report.txt) \
+    | sed 's/ //g' \
+    | bioawk -tc header 'NR>1 {print $GenBank_Accn,$Sequence_Length}' \
+    | sed -e "s/CM000663.1/1/g" \
+    | sed -e "s/CM000664.1/2/g" \
+    | sed -e "s/CM000665.1/3/g" \
+    | sed -e "s/CM000666.1/4/g" \
+    | sed -e "s/CM000667.1/5/g" \
+    | sed -e "s/CM000668.1/6/g" \
+    | sed -e "s/CM000669.1/7/g" \
+    | sed -e "s/CM000670.1/8/g" \
+    | sed -e "s/CM000671.1/9/g" \
+    | sed -e "s/CM000672.1/10/g" \
+    | sed -e "s/CM000673.1/11/g" \
+    | sed -e "s/CM000674.1/12/g" \
+    | sed -e "s/CM000675.1/13/g" \
+    | sed -e "s/CM000676.1/14/g" \
+    | sed -e "s/CM000677.1/15/g" \
+    | sed -e "s/CM000678.1/16/g" \
+    | sed -e "s/CM000679.1/17/g" \
+    | sed -e "s/CM000680.1/18/g" \
+    | sed -e "s/CM000681.1/19/g" \
+    | sed -e "s/CM000682.1/20/g" \
+    | sed -e "s/CM000683.1/21/g" \
+    | sed -e "s/CM000684.1/22/g" \
+    | sed -e "s/CM000685.1/X/g" \
+    | sed -e "s/CM000686.1/Y/g" \
+    | sed -e "s/J01415.2/MT/g" > chromlength.txt
+
+## Save the report file with a header
+cat <(grep -E "^[1-9]+|^[X]|^[Y]|^(MT)" chromlength.txt | sort -k1,1) \
+    <(grep -E -v "^[1-9]+|^[X]|^[Y]|^(MT)" chromlength.txt | sort -k2,2) \
+    | sed "s/\r//g" \
+    | awk 'BEGIN {print "#chrom\tlength"}  { print $1"\t"$2}' > GRCh37.genome
+
+
+## Remove extra files
+rm chromlength.txt
+rm GCA_000001405.14_GRCh37.p13_assembly_report.txt
