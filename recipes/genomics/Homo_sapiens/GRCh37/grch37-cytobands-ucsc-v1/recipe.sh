015a24c7a885c8b459af5e3b33ce44c491c0bdee
Michael Cormier <cormiermichaelj@gmail.com>
2020-04-16 10:35:20 -0600

    Updated and new cytoband recipes from UCSC

--- recipes/genomics/Homo_sapiens/GRCh37/grch37-cytobands-ucsc-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-cytobands-ucsc-v1/recipe.sh
@@ -1,24 +1,19 @@
 #!/bin/sh
 set -eo pipefail -o nounset
+
 ## Get the ggd genome file
 genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/GRCh37/GRCh37.genome
 
-## Create the remap sed file
-##  UCSC <-> Ensembl <->  mapping at: https://github.com/dpryan79/ChromosomeMappings  
-##  Any unmapped regions will be dleted 
-wget --quiet -O - https://raw.githubusercontent.com/dpryan79/ChromosomeMappings/master/GRCh37_UCSC2ensembl.txt \
-    | awk '{if(NF==2) print "s/^"$1"/"$2"/g"; else if(NF==1) print "/^"$1"/d"}' | sort -r > remap.sed
+## Get the chromomsome mapping file
+chrom_mapping=$(ggd get-files grch37-chrom-mapping-ucsc2ensembl-ncbi-v1 --pattern "*.txt")
 
 # download, header, remap chroms, sort, bgzip
 wget --quiet -O - http://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/cytoBand.txt.gz \
     | gzip -dc \
-    | awk -v OFS="\t" 'BEGIN {print "#chrom\tstart\tend\tband\tstain"} {print $1,$2,$3,$4,$5}' \
-    | sed -f remap.sed \
-    | gsort /dev/stdin $genome \
+    | awk -v OFS="\t" 'BEGIN {print "#Table Info: http://genome.ucsc.edu/cgi-bin/hgTables?db=hg19&hgta_group=map&hgta_track=cytoBand&hgta_table=cytoBand&hgta_doSchema=describe+table+schema\n#chrom\tstart\tend\tband\tstain"} {print $1,$2,$3,$4,$5}' \
+    | gsort --chromosomemappings $chrom_mapping /dev/stdin $genome \
     | bgzip -c > grch37-cytoband-ucsc-v1.bed.gz
 
 # index the bed file using tabix
 tabix grch37-cytoband-ucsc-v1.bed.gz
 
-# cleanup
-rm remap.sed
--- recipes/genomics/Homo_sapiens/GRCh38/grch38-cytobands-ucsc-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh38/grch38-cytobands-ucsc-v1/recipe.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 set -eo pipefail -o nounset
-## Get the ggd genome file
-genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/hg38/hg38.genome
 
-# download, header, sort, bgzip
+## Get the ggd genome file
+genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/GRCh38/GRCh38.genome
+
+## Get the chromomsome mapping file
+chrom_mapping=$(ggd get-files grch38-chrom-mapping-ucsc2ensembl-ncbi-v1 --pattern "*.txt")
+
+# download, header, remap chroms, sort, bgzip
 wget --quiet -O - http://hgdownload.cse.ucsc.edu/goldenpath/hg38/database/cytoBand.txt.gz \
     | gzip -dc \
     | awk -v OFS="\t" 'BEGIN {print "#Table Info: http://genome.ucsc.edu/cgi-bin/hgTables?db=hg38&hgta_group=map&hgta_track=cytoBand&hgta_table=cytoBand&hgta_doSchema=describe+table+schema\n#chrom\tstart\tend\tband\tstain"} {print $1,$2,$3,$4,$5}' \
-    | gsort /dev/stdin $genome \
-    | bgzip -c > hg38-cytoband-ucsc-v1.bed.gz
+    | gsort --chromosomemappings $chrom_mapping /dev/stdin $genome \
+    | bgzip -c > grch38-cytoband-ucsc-v1.bed.gz
 
 # index the bed file using tabix
-tabix hg38-cytoband-ucsc-v1.bed.gz
+tabix grch38-cytoband-ucsc-v1.bed.gz
+
