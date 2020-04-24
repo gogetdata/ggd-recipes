a8cd56be55d7cc9913ccf0a48f0e857172f241c2
Michael Cormier <35238534+mikecormier@users.noreply.github.com>
2020-04-24 12:42:56 -0600

    Update Microsatellites (#102)
    * Remove cytoband bed file from recipe dir
    
    * Update microsatellite recipes

--- recipes/genomics/Homo_sapiens/hg19/hg19-microsatellites-ucsc-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/hg19/hg19-microsatellites-ucsc-v1/recipe.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -eo pipefail -o nounset
+
+
 genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/hg19/hg19.genome
+
 wget --quiet -O - http://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/microsat.txt.gz \
     | gzip -dc \
-    | awk -v OFS="\t" 'BEGIN {print "#chrom\tstart\tend\ttype"} {print $2,$3,$4,$5}' \
+    | awk -v OFS="\t" 'BEGIN {print "#Table Info: https://genome.ucsc.edu/cgi-bin/hgTables?db=hg19&hgta_group=rep&hgta_track=microsat&hgta_table=microsat&hgta_doSchema=describe+table+schema\n#chrom\tstart\tend\ttype"} {print $2,$3,$4,$5}' \
     | gsort /dev/stdin $genome \
     | bgzip -c > hg19-microsat-ucsc-v1.bed.gz
+
 tabix hg19-microsat-ucsc-v1.bed.gz
