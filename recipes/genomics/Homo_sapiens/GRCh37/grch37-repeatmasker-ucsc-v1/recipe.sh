b144ba63bb6342837200ad7d6505de0daf3b81cd
Michael Cormier <cormiermichaelj@gmail.com>
2020-04-27 17:02:18 -0600

    Repeat Masker recipes from UCSC

--- recipes/genomics/Homo_sapiens/GRCh37/grch37-repeatmasker-ucsc-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-repeatmasker-ucsc-v1/recipe.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -eo pipefail -o nounset
 
-
 ## Get the genome file
 genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/hg19/hg19.genome
+genome2=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/GRCh37/GRCh37.genome
 wget --quiet $genome
+
+## Get the chromosome mapping file
+chrom_mapping=$(ggd get-files grch37-chrom-mapping-ucsc2ensembl-ncbi-v1 --pattern "*.txt")
 
 ## Get repeat masker file
 wget --quiet http://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/rmsk.txt.gz \
@@ -21,11 +24,11 @@ myArray=($(zcat rmsk.txt.gz | cut -f 6 | sort | uniq | awk ' { if (system("grep 
 gzip -dc rmsk.txt.gz \
     | awk -v bashArray="${myArray[*]}" 'BEGIN{ split(bashArray,chromList); for (i in chromList) chromDict[chromList[i]] = chromList[i] } { if ( !($6 in chromDict) ) print $0 }' \
     | awk -v OFS="\t" 'BEGIN {print "#Table Info: https://genome.ucsc.edu/cgi-bin/hgTables?db=hg19&hgta_group=rep&hgta_track=rmsk&hgta_table=rmsk&hgta_doSchema=describe+table+schema\n#chrom\tstart\tend\tstrand\trelativeStart\trelativeEnd\trepeatName\trepeatClass\trepeatFamily\tclass_family_name\tdiv\tdel\tins\tdiv+del+ins"} { print $6,$7,$8,$10,$14,$15,$11,$12,$13,$12"_"$13"_"$11,$3,$4,$5,$3+$4+$5}' \
-    | gsort /dev/stdin hg19.genome \
-    | bgzip -c > hg19-repeatmasker-ucsc-v1.bed.gz
+    | gsort --chromosomemappings $chrom_mapping /dev/stdin $genome2 \
+    | bgzip -c > grch37-repeatmasker-ucsc-v1.bed.gz
 
-tabix hg19-repeatmasker-ucsc-v1.bed.gz
+tabix grch37-repeatmasker-ucsc-v1.bed.gz
 
+## Remove extra files
 rm hg19.genome
 rm rmsk.txt.gz
-
