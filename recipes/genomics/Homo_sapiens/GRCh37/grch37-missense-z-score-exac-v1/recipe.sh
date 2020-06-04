0b347206d088eef4243665ee96d1cf1805b46391
Michael Cormier <cormiermichaelj@gmail.com>
2020-06-04 11:56:39 -0600

    Fix header

--- recipes/genomics/Homo_sapiens/GRCh37/grch37-missense-z-score-exac-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-missense-z-score-exac-v1/recipe.sh
@@ -8,7 +8,7 @@ genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Ho
 ## Get the missense z score from the gnomad donwload site
 wget --quiet -O - https://storage.googleapis.com/gnomad-public/legacy/exac_browser/forweb_cleaned_exac_r03_march16_z_data_pLI_CNV-final.txt.gz \
     | gzip -dc \
-    | awk -v OFS="\t" 'BEGIN {print "#chrom\ttxStart\ttxEnd\tgene_symbol\ttranscript_id\texon_number\tnbps\tmissense_z_score"} 
+    | awk -v OFS="\t" 'BEGIN {print "#chrom\ttxStart\ttxEnd\tgene_symbol\ttranscript_id\texon_number\tn_bp\tmissense_z_score"} 
                              { if (NR > 1) print $3,$5,$6,$2,$1,$4,$7,$18}' \
     | gsort /dev/stdin $genome \
     | uniq \
--- recipes/genomics/Homo_sapiens/hg19/hg19-missense-z-score-exac-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/hg19/hg19-missense-z-score-exac-v1/recipe.sh
@@ -10,7 +10,7 @@ chr_mapping=$(ggd get-files hg19-chrom-mapping-ensembl2ucsc-ncbi-v1 --pattern "*
 ## Get the missense z score from the gnomad donwload site
 wget --quiet -O - https://storage.googleapis.com/gnomad-public/legacy/exac_browser/forweb_cleaned_exac_r03_march16_z_data_pLI_CNV-final.txt.gz \
     | gzip -dc \
-    | awk -v OFS="\t" 'BEGIN {print "#chrom\ttxStart\ttxEnd\tgene_symbol\ttranscript_id\texon_number\tn_bp"} 
+    | awk -v OFS="\t" 'BEGIN {print "#chrom\ttxStart\ttxEnd\tgene_symbol\ttranscript_id\texon_number\tn_bp\tmissense_z_score"} 
                              { if (NR > 1) print $3,$5,$6,$2,$1,$4,$7,$18}' \
     | gsort --chromosomemappings $chr_mapping /dev/stdin $genome \
     | uniq \
