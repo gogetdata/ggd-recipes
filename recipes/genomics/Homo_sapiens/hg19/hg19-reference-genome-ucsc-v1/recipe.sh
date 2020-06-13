22a54159905e210e2f62d1e6c62dbdc6fc4c9128
Michael Cormier <cormiermichaelj@gmail.com>
2020-06-13 10:00:29 -0600

    Update UCSC reference genomes

--- recipes/genomics/Homo_sapiens/hg19/hg19-reference-genome-ucsc-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/hg19/hg19-reference-genome-ucsc-v1/recipe.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -eo pipefail -o nounset
-## Get the latest hg19 reference genome for USCS. (Patch 13) 
-wget --quiet -O hg19-reference-genome-ucsc-v1.fa.gz http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz
 
-## Unzip file using gzip
-gzip -fd hg19-reference-genome-ucsc-v1.fa.gz
+## Get the latest hg19 reference genome for USCS. (Patch 13) 
+wget --quiet -O - http://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/p13.plusMT/hg19.p13.plusMT.fa.gz \
+    | gzip -dc \
+    | bgzip -c > hg19-reference-genome-ucsc-v1.fa.gz 
 
 ## Index the fasta file using samtools 
-samtools faidx hg19-reference-genome-ucsc-v1.fa
+samtools faidx hg19-reference-genome-ucsc-v1.fa.gz
