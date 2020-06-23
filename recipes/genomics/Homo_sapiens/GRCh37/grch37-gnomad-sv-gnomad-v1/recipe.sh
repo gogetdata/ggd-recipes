7c53cb8500d6703607fbffdb496457d1f807af8d
jbelyeu <jrbelyeu@gmail.com>
2020-06-23 15:01:15 -0600

    added gnomad-sv recipes

---
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-gnomad-sv-gnomad-v1/recipe.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eo pipefail -o nounset
+#download SVs from gnomAD (grch37) in bgzipped VCF format
+wget --quiet https://storage.googleapis.com/gnomad-public/papers/2019-sv/gnomad_v2.1_sv.sites.vcf.gz 
+
+#sort and tabix
+gsort gnomad_v2.1_sv.sites.vcf.gz https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/GRCh37/GRCh37.genome | bgzip -c > gnomad_v2_sv_sorted.sites.vcf.gz
+tabix gnomad_v2_sv_sorted.sites.vcf.gz
+
+#clean up
+rm gnomad_v2.1_sv.sites.vcf.gz
