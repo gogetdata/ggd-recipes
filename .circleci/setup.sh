1d1c617114359f729143d8374c8e22dd0b39a2a6
Michael Cormier <cormiermichaelj@gmail.com>
2020-06-25 11:08:40 -0600

    Update bioconda utils install

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -79,7 +79,7 @@ if [[ ! -d $WORKSPACE/anaconda ]]; then
     #$WORKSPACE/anaconda/bin/conda install -y --file https://raw.githubusercontent.com/bioconda/bioconda-utils/$BIOCONDA_UTILS_TAG/bioconda_utils/bioconda_utils-requirements.txt
     conda install -y --file https://raw.githubusercontent.com/bioconda/bioconda-utils/$BIOCONDA_UTILS_TAG/bioconda_utils/bioconda_utils-requirements.txt
     #$WORKSPACE/anaconda/bin/pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG
-    pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG
+    $WORKSPACE/anaconda/envs/check-ggd-recipe/bin/pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG
 
     # step 5: cleanup
     conda clean -y --all
