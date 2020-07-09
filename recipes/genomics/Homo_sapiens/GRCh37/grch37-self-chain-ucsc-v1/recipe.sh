b39adf084418066e73c883f153591c3bce6879af
Michael Cormier <cormiermichaelj@gmail.com>
2020-07-09 16:25:38 -0600

    debug

--- recipes/genomics/Homo_sapiens/GRCh37/grch37-self-chain-ucsc-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-self-chain-ucsc-v1/recipe.sh
@@ -8,15 +8,17 @@ chrom_mapping=$(ggd get-files grch37-chrom-mapping-ucsc2ensembl-ncbi-v1 --patter
 awk '{if (NR > 1) print "s/"$1"/"$2"/g"} ' $chrom_mapping \
     | sort -r > remap.sed
 
+cat remap.sed
+
 wget --quiet -O - http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/chainSelf.txt.gz \
     | gzip -dc \
     | awk -v OFS="\t" 'BEGIN {print "#Table Info: https://genome.ucsc.edu/cgi-bin/hgTables?db=hg19&hgta_group=rep&hgta_track=chainSelf&hgta_table=chainSelf&hgta_doSchema=describe+table+schema\n#target_chrom\ttarget_start\ttarget_end\ttarget_size\tquery_chrom\tquery_start\tquery_end\tquery_size\tquery_strand\tchain_id\tscore\tnormalized_score"} 
                              {print $3,$5,$6,$4,$7,$10,$11,$8,$9,$12,$2,$13}' \
     | sed -f remap.sed \
-    | gsort /dev/stdin $genome \
-    | bgzip -c > grch37-selfchain-ucsc-v1.bed.gz
+    #| gsort /dev/stdin $genome \
+    #| bgzip -c > grch37-selfchain-ucsc-v1.bed.gz
 
-tabix grch37-selfchain-ucsc-v1.bed.gz
+#tabix grch37-selfchain-ucsc-v1.bed.gz
 
 rm remap.sed
 
