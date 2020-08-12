d65724ffeaddda9acd0fd45628c5690d8b70788b
Jon Belyeu <jrbelyeu@gmail.com>
2020-08-11 22:16:41 -0600

    added recipes for dbvar SVs (#146)
    * added recipes for dbvar SVs
    
    * updated file size
    
    * resized files i guess
    
    * removed incorrectly placed files
    
    * rebuilt on linux
    
    * aaaand fixed this one

---
+++ recipes/genomics/Homo_sapiens/GRCh37/grch37-structural-variants-database-genomic-variants-ucsc-v1/recipe.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eo pipefail -o nounset
+wget -q http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/dgvSupporting.txt.gz
+genome="https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/GRCh37/GRCh37.genome"
+chrom_mapping=$(ggd get-files grch37-chrom-mapping-ucsc2ensembl-ncbi-v1 --pattern "*.txt")
+
+printf "#chrom\tchromStart\tchromEnd\tname\tscore\tstrand\tthickStart\tthickEnd\titemRgb\tvarType\treference\tpubMedId\tmethod\tplatform\tmergedVariants\tsupportingVariants\tsampleSize\tobservedGains\tobservedLosses\tcohortDescription\tgenes\tsamples\t_size" > dgvSupporting.bed
+gunzip -c dgvSupporting.txt.gz | cut -f 2- >> dgvSupporting.bed
+
+gsort --chromosomemappings $chrom_mapping dgvSupporting.bed $genome | bgzip -c > dgvSupporting.bed.gz
+tabix dgvSupporting.bed.gz
+rm dgvSupporting.txt.gz
+rm dgvSupporting.bed
