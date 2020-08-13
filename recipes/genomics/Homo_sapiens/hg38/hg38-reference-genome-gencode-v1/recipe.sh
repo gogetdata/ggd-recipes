917207cad824a57975a600351b0d7af29d6ab1bf
jbelyeu <jrbelyeu@gmail.com>
2020-08-12 18:43:06 -0600

    changed references to primary assembly

--- recipes/genomics/Homo_sapiens/hg38/hg38-reference-genome-gencode-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/hg38/hg38-reference-genome-gencode-v1/recipe.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -eo pipefail -o nounset
+#!/bin/sh
+set -eo pipefail -o nounset
 
-wget --quiet -O - ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_34/GRCh38.p13.genome.fa.gz\
+wget --quiet -O - ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_34/GRCh38.primary_assembly.genome.fa.gz\
     | gzip -dc \
     | bgzip -c > hg38-reference-genome-gencode-v1.fa.gz
 
