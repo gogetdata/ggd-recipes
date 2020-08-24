4b97b3058c2ca112152d3c6b35ca413ee8ad65c2
Michael Cormier <35238534+mikecormier@users.noreply.github.com>
2020-08-24 13:38:43 -0600

    Gene Only Feature Recipes (#163)

---
+++ recipes/genomics/Homo_sapiens/hg38/hg38-gene-only-features-ensembl-v1/recipe.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eo pipefail -o nounset
+#!/bin/sh
+set -eo pipefail -o nounset
+
+
+## Get .genome file
+genome=https://raw.githubusercontent.com/gogetdata/ggd-recipes/master/genomes/Homo_sapiens/hg38/hg38.genome
+
+## Get the chromomsome mapping file
+chr_mapping=$(ggd get-files hg38-chrom-mapping-ensembl2ucsc-ncbi-v1 --pattern "*.txt")
+
+## Process GTF file
+wget --quiet ftp://ftp.ensembl.org/pub/release-99/gtf/homo_sapiens/Homo_sapiens.GRCh38.99.gtf.gz 
+
+cat <(gzip -dc Homo_sapiens.GRCh38.99.gtf.gz | grep "^#") <(gzip -dc Homo_sapiens.GRCh38.99.gtf.gz | grep -v "^#") \
+    | awk -v OFS="\t" 'BEGIN{print "#chrom\tsource\tfeature\tstart\tend\tscore\tstrand\tframe\tattribute"} { if ( $3 == "gene") print $0}' \
+    | gsort --chromosomemappings $chr_mapping /dev/stdin $genome \
+    | bgzip -c > hg38-gene-only-features-ensembl-v1.gtf.gz
+
+tabix hg38-gene-only-features-ensembl-v1.gtf.gz
+
+rm Homo_sapiens.GRCh38.99.gtf.gz
