a500058025452a6ea049a39e3fbe17f4b6dd8c3b
Michael Cormier <cormiermichaelj@gmail.com>
2020-10-07 15:22:55 -0600

    Fix appris tags during filtering

--- recipes/genomics/Homo_sapiens/GRCh38/grch38-canonical-transcript-features-gencode-v1/recipe.sh
+++ recipes/genomics/Homo_sapiens/GRCh38/grch38-canonical-transcript-features-gencode-v1/recipe.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -eo pipefail -o nounset
 
-
-
 cat << EOF > get_canonical_transcripts.py
 
 import sys
@@ -36,22 +34,21 @@ def score_appris(appris_level):
     Get a score between 0-7 based on the appris level
     """
     
-    if appris_level == "PRINCIPAL:1":
+    if appris_level == "appris_principal_1":
         return(1)
-    if appris_level == "PRINCIPAL:2":
+    if appris_level == "appris_principal_2":
         return(2)
-    if appris_level == "PRINCIPAL:3":
+    if appris_level == "appris_principal_3":
         return(3)
-    if appris_level == "PRINCIPAL:4":
+    if appris_level == "appris_principal_4":
         return(4)
-    if appris_level == "PRINCIPAL:5":
+    if appris_level == "appris_principal_5":
         return(5)
-    if appris_level == "ALTERNATIVE:1":
+    if appris_level == "appris_alternative_1":
         return(6)
-    if appris_level == "ALTERNATIVE:2":
+    if appris_level == "appris_alternative_2":
         return(7)
     return(1000)
-
 
 
 def best_appris(first_appris, second_appris):
@@ -90,7 +87,7 @@ for line in fh:
     
     line_dict = dict(zip(header,line.strip().split("\t")))
     line_dict.update({x.strip().replace("\"","").split(" ")[0]:x.strip().replace("\"","").split(" ")[1] for x in line_dict["attribute"].strip().split(";")[:-1]})
-    
+
     ## Only look at transcripts
     if line_dict["feature"] == "transcript":
         ## Get only protein-coding only biotype 
@@ -181,7 +178,7 @@ gzip -dc gencode.v34.chr_patch_hapl_scaff.annotation.gtf.gz \
     | grep -v "^#" \
     | awk -v OFS="\t" 'BEGIN {print "chrM\tMT"} { if ( $1 != "chrM") gsub("chr","", $1); print "chr"$1, $1}' \
     | uniq >  chrom_mapping.txt
-
+    
 python get_canonical_transcripts.py gencode.v34.chr_patch_hapl_scaff.annotation.gtf.gz \
     | gsort --chromosomemappings chrom_mapping.txt /dev/stdin $genome \
     | bgzip -c > grch38-canonical-transcripts-gencode-v1.gtf.gz
